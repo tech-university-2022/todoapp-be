8a4c6b19da382d947081078e912295551f5bfac5
Thuc Le <lethuc.crma@gmail.com>
2022-07-22 16:47:39 +0700

    feat: test catch async

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,15 @@ module.exports = {
   },
   restoreMocks: true,
   rootDir: './',
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/index.js', 'tests'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'src/config',
+    'src/index.js',
+    'tests',
+    'src/services/todo.service.js',
+    'src/models',
+    'database/models',
+    'test',
+  ],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
