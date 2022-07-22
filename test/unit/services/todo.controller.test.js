54fecb1acc643d996d8de5b551c46d1717cdd21a
duongcscx <duongcscx@gmail.com>
2022-07-22 13:48:55 +0700

    fix: create data for  test

--- test/unit/services/todo.controller.test.js
+++ test/unit/services/todo.controller.test.js
@@ -1,14 +1,12 @@
 const httpStatus = require('http-status');
 const TodoController = require('../../../src/controllers/todo.controller');
-// const sendResponse = require('../../../src/utils/sendResponse');
-const sendError = require('../../../src/utils/sendError');
 const todoService = require('../../../src/services/todo.service');
 const logger = require('../../../src/configs/logger');
 
-// jest.mock('../../../src/utils/sendResponse');
-jest.mock('../../../src/utils/sendError');
 jest.mock('../../../src/services/todo.service');
 jest.mock('../../../src/configs/logger');
+
+const { AUTHs, TODOs } = require('./todo.test.data');
 
 describe('Test API: get many todos', () => {
   beforeEach(() => {
@@ -18,16 +16,14 @@ describe('Test API: get many todos', () => {
   const res = {
     send: jest.fn((data, message) => ({ message, ...data })),
     json: jest.fn((status, message) => ({ status, message })),
+    // status: jest.fn((statusCode) => ({
+    //   json: jest.fn((status, message) => ({ status, message })),
+    // })),
   };
 
-  test('TC1 - Empty userId', () => {
+  test('TC1 - 0 rows', () => {
     const req = {
-      authorized: {
-        user: {
-          email: 'TC1@mail.com',
-          id: 'TC1',
-        },
-      },
+      authorized: AUTHs[2],
       query: {
         take: 10,
         page: 1,
@@ -66,12 +62,7 @@ describe('Test API: get many todos', () => {
 
   test('TC2 - Return 1 row', () => {
     const req = {
-      authorized: {
-        user: {
-          email: 'TC1@mail.com',
-          id: 'TC1',
-        },
-      },
+      authorized: AUTHs[1],
       query: {
         take: 10,
         page: 1,
@@ -79,24 +70,44 @@ describe('Test API: get many todos', () => {
     };
 
     const queryResult = {
-      todos: [
-        {
-          id: 'defe5f10-2469-4eb7-911d-d930f2e1a5a7',
-          title: 'Test 2',
-          content: 'Example content 2',
-          createdBy: 'e60892d8-0359-11ed-b939-0242ac120002',
-          status: 'Pending',
-          dueDate: '2022-07-24 15:47:08.02059+00',
-          createdAt: '2022-07-14 15:51:41.154884+00',
-          updatedAt: '2022-07-14 15:51:41.154884+00',
-          categories: null,
-        },
-      ],
+      todos: [TODOs[2]],
       pagination: {
         take: 10,
         page: 1,
         haveNextPage: false,
         total: 1,
+      },
+    };
+
+    const correctedResult = {
+      message: 'OK',
+      data: {
+        ...queryResult,
+      },
+    };
+
+    todoService.getManyTodos.mockResolvedValue(queryResult);
+
+    return TodoController.getManyTodos(req, res).then((result) => {
+      return expect(result).toEqual(correctedResult);
+    });
+  });
+  test('TC3 - Return 2 rows', () => {
+    const req = {
+      authorized: AUTHs[1],
+      query: {
+        take: 10,
+        page: 1,
+      },
+    };
+
+    const queryResult = {
+      todos: [TODOs[0], TODOs[1]],
+      pagination: {
+        take: 10,
+        page: 1,
+        haveNextPage: false,
+        total: 2,
       },
     };
 
---
+++ test/unit/services/todo.test.data.js
@@ -0,0 +1,58 @@
+const AUTHs = [
+  {
+    user: {
+      email: 'user1@mail.com',
+      id: 'user1',
+    },
+  },
+  {
+    user: {
+      email: 'user2@mail.com',
+      id: 'user2',
+    },
+  },
+  {
+    user: {
+      email: 'user3@mail.com',
+      id: 'user3',
+    },
+  },
+];
+
+const TODOs = [
+  {
+    id: 'Todo 1',
+    title: 'Title 1',
+    content: 'Content 1',
+    createdBy: 'user1',
+    status: 'Done',
+    dueDate: '2022-07-24 15:47:08.02059+00',
+    createdAt: '2022-07-14 15:51:41.154884+00',
+    updatedAt: '2022-07-14 15:51:41.154884+00',
+    categories: 'Work',
+  },
+  {
+    id: 'Todo 2',
+    title: 'Title 2',
+    content: 'Content 2',
+    createdBy: 'user1',
+    status: 'Pending',
+    dueDate: '2022-07-24 15:47:08.02059+00',
+    createdAt: '2022-07-14 15:51:41.154884+00',
+    updatedAt: '2022-07-14 15:51:41.154884+00',
+    categories: 'Play',
+  },
+  {
+    id: 'Todo 3',
+    title: 'Title 3',
+    content: 'Content 3',
+    createdBy: 'user2',
+    status: 'Pending',
+    dueDate: '2022-07-24 15:47:08.02059+00',
+    createdAt: '2022-07-14 15:51:41.154884+00',
+    updatedAt: '2022-07-14 15:51:41.154884+00',
+    categories: 'Play',
+  },
+];
+
+module.exports = { AUTHs, TODOs };
