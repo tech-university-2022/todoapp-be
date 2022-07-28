3c5637eda255870efe7c2198bf71ef7f706337af
Thuc Le <lethuc.crma@gmail.com>
2022-07-28 14:24:04 +0700

    feat: github action build phase

--- database/config/config.js
+++ database/config/config.js
@@ -1,3 +1,9 @@
+console.log({
+  url: `postgres://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${
+    process.env.DB_PORT
+  }/${process.env.DB_NAME}`,
+  dialect: 'postgres',
+});
 module.exports = {
   development: {
     url: `postgres://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${
